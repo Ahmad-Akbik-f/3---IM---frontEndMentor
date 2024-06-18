b8a9996f0b6b91da6da56bfb0827d5f8bad277d5
Ahmad Akbik <fadomado234@gmail.com>
2024-06-18 13:53:55 +0300

    Some Upgrades but it is still bad

--- JS/main.js
+++ JS/main.js
@@ -6,7 +6,6 @@ let dCard = document.querySelector(`.cards .days .theCard`)
 let mCard = document.querySelector(`.cards .min .theCard`)
 let hCard = document.querySelector(`.cards .hr .theCard`)
 let sCard = document.querySelector(`.cards .sec .theCard`)
-let S = new Date().getSeconds();
 //Vars
 let sec =  0;
 let hr = 0; 
@@ -36,11 +35,56 @@ hCnt.forEach((i)=>{
         i.innerHTML = "0" + hr
     }
 })
-
+if(hr > 0){
+    if(hr < 10){
+        hCnt[1].innerHTML = "0" + hr - 1
+    }else{
+        hCnt[1].innerHTML = hr - 1
+    }
+}else{
+    hCnt[1].innerHTML = 59
+}
+if(min > 0){
+    if(min < 10){
+        mCnt[1].innerHTML = "0" + min - 1
+    }else{
+        mCnt[1].innerHTML = min - 1
+    }
+}else{
+    mCnt[1].innerHTML = 59
+}
+if(sec > 0){
+    if(sec < 10){
+        sCnt[1].innerHTML = "0" + sec - 1
+    }else{
+        sCnt[1].innerHTML = sec - 1
+    }
+}else{
+    sCnt[1].innerHTML = 59
+}
+if(days > 0){
+    if(days < 10){
+        dCnt[1].innerHTML = "0" + days - 1
+    }else{
+        dCnt[1].innerHTML = days - 1
+    }
+}
 //
 
+if(sec == 0){
+    mCard.classList.add('flip')
+}
+if(min == 0 && sec == 0){
+    hCard.classList.add('flip')
+}
+if(min == 0 && sec == 0 && hr == 0){
+    dCard.classList.add('flip')
+}
+//
+
+
 let cnt = setInterval(showTime,1000)
-console.log(sCard)
+
 function showTime() {
     sCard.classList.remove('flip')
     mCard.classList.remove('flip')
@@ -49,13 +93,15 @@ function showTime() {
     if(days == 0 && min == 0 && sec == 0 && hr == 0){
         clearInterval(cnt)
     }else{
+        sec--;
         if(sec > 0){
             if(!sCard.classList.contains('flip')){
                 sCard.classList.add('flip')
+
             }
-            sec--;
             sCnt.forEach((i)=>{
                 i.innerHTML = sec;
+                sCnt[1].innerHTML = sec - 1 
             })
             if(sec < 10){
                 sCnt.forEach((i)=>{
@@ -63,20 +109,17 @@ function showTime() {
                 })
             }
         }else{
-            if(!sCard.classList.contains('flip')){
-                sCard.classList.add('flip')
-            }
+            min--
+            mCard.classList.add('flip')
+            sCard.classList.add('flip')
             sec = 59;
             sCnt.forEach((i)=>{
-                i.innerHTML = sec;
+                i.innerHTML = 59;
             })
             if(min > 0){
-                if(!mCard.classList.contains('flip')){
-                    mCard.classList.add('flip')
-                }
-                min--
                 mCnt.forEach((i)=>{
                     i.innerHTML = min;
+                    mCnt[1].innerHTML = min - 1
                 })
                 if(min < 10){
                     mCnt.forEach((i)=>{
@@ -84,37 +127,28 @@ function showTime() {
                     })
                 }
             }else{
-                if(!mCard.classList.contains('flip')){
-                    mCard.classList.add('flip')
-                }
+                hCard.classList.add('flip')
                 mCard.classList.add('flip')
                 min = 59 ;
                 mCnt.forEach((i)=>{
                     i.innerHTML = min;
                 })
+                hr--
                 if(hr > 0){
-                    if(!hCard.classList.contains('flip')){
-                        hCard.classList.add('flip')
-                    }
-                    hr--
                     if(hr < 10){
                         hCnt.forEach((i)=>{
                             i.innerHTML = "0" + hr;
                         })
                     }
                 }else{
-                    if(!hCard.classList.contains('flip')){
-                        hCard.classList.add('flip')
-                    }
+                    hCard.classList.add('flip')
+                    dCard.classList.add('flip')
                     hr = 23;
                     hCnt.forEach((i)=>{
                         i.innerHTML = hr;
                     })
+                    days--
                     if(days > 0){
-                        if(!dCard.classList.contains('flip')){
-                            dCard.classList.add('flip')
-                        }
-                        days--
                         dCnt.forEach((i)=>{
                             i.innerHTML = days;
                         })
